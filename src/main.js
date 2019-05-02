61ebc0259b5a0750743e65db43fde1e01e0eabf5
agussalberdi <agustinalberdi1@gmail.com>
2019-05-02 19:40:06 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,65 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import Vuex from 'vuex'
+import axios from 'axios'
 
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    username: ''
+  },
+  mutations: {
+    username (state,name) {
+      state.username = name;
+    }
+  },
+  actions: {
+    update_user_name(store,name)
+    {
+      store.commit('username',name);
+    }
+  }
+})
+
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  created:function(){
+  	this.checkLogin()
+  },
+  watch:{
+  	'$route':'checkLogin'
+  },
+  router,
+  store,
+  template: '<App/>',
+  components: { App },
+  methods:{
+  	checkLogin(){
+      
+  		if(!localStorage.getItem("login"))
+      {
+        this.$router.push('login');
+        return;
+      } 
+
+      if(!this.$store.username && localStorage.getItem('login'))
+      {
+        this.$store.dispatch('update_user_name',this.$store.state.username);
+        this.$router.push('user');
+        return;
+      }
+
+      if(this.$store.username && localStorage.getItem('login'))
+      {
+         this.$router.push('user');
+         return;
+      }
+  	}
+  },
+  render: h => h(App)
+})
